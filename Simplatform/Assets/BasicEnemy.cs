de033e4e0e9d803b5a69d8af035f4ea7713c773a
astora83 <62930880+astora83@users.noreply.github.com>
2020-10-02 10:38:20 -0500

    ....

---
+++ Simplatform/Assets/BasicEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] targets;
+    int targetIndex = 0;
+    Transform currenTarget;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, currenTarget.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currenTarget.position) < dist)
+        {
+            targetIndex++;
+            if (targetIndex >= targets.Length)
+                targetIndex = 0;
+
+            UpdateTarget();
+        }
+    }
+
+    void UpdateTarget()
+    {
+        currenTarget = targets[targetIndex];
+    }
+}
